68503ccca9d1499835ac0047c94b949d132b1b56
A.Volovod <avolovod@brightgrove.com>
2020-05-02 14:52:48 +0300

    add MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const mongoose = require('mongoose')
 const parser = require('./parser')
 const utils = require('./utils/utils')
-const keys = require('/config/keys')
+const keys = require('./config/keys')
 
  let cashed = []
 async function start() {
     try {
+        await mongoose.connect(keys.mongoURL,
+            { useNewUrlParser: true,  useUnifiedTopology: true, useCreateIndex : true })
+            .then(() =>{
+                // TODO load cashed
+            })
+            .catch(error => console.log(`MongoDB connection problem ${error}`))
+
         const categories = await parser(keys.fullURL());
         const newestCategories = utils.getNewestCategories(categories, cashed)
     } catch (e) {
